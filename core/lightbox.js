e94b0ddfeffae864e6c72f88e81a6a5496489e93
fargelus <ddraudred@gmail.com>
2019-01-20 21:35:00 +0300

    Animating img move to center

--- core/lightbox.js
+++ core/lightbox.js
@@ -1,16 +1,42 @@
 $(() => {
-  $('a[data-lightbox]').click((ev) => {
-    $('<div>').attr({
-      id: 'overlay',
-      class: 'overlay'
-    }).prependTo('body');
+  $('a[data-lightbox]').click(function(ev) {
+    const $lightboxCont = $('<div>').attr({
+      id: 'lightbox-container',
+      class: 'lightbox-container',
+    });
 
-    const $self = $(ev.currentTarget);
-    $self.addClass('active');
+    const $innerImage = $(this).find('img');
+    const innerImageCoords = $innerImage.offset();
+    const $lightBoxImg = $('<img class="lightbox-img">')
+      .css({
+        left: innerImageCoords.left,
+        top: innerImageCoords.top,
+      })
+      .data({
+        initLeft: innerImageCoords.left,
+        initTop: innerImageCoords.top,
+      })
+      .animate({
+        left: '50%',
+        top: '50%',
+      }, 350)
+      .attr('src', $(this).attr('href') )
+      .appendTo($lightboxCont);
 
-    const $img = $self.children();
-    $img.attr('src', $self.attr('href'));
-
+    $('body').append($lightboxCont);
     return false;
+  });
+
+  $(document).on('click', '#lightbox-container', (ev) => {
+    const $self = $(ev.target);
+
+    const $lightBoxImg = $self.find('.lightbox-img');
+    // TODO анимация стартует одновременно для контейнера и картинки
+    $lightBoxImg.animate({
+      left: $lightBoxImg.data('initLeft'),
+      top: $lightBoxImg.data('initTop'),
+    }, () => {
+        $self.remove();
+    });
   });
 });
