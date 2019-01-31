6f29352f66acd3120d08752a99f101f23bd645c0
fargelus <ddraudred@gmail.com>
2019-01-31 08:23:39 +0300

    Gulp init

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+
+const del = require('del');
+
+const sourcemaps = require('gulp-sourcemaps');
+const stylus = require('gulp-stylus');
+const rename = require('gulp-rename');
+
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+
+const paths = {
+  styles: './src/styl/*.styl',
+  scripts: './src/js/*.js',
+  dest: './dist'
+};
+
+gulp.task('clean', () => {
+  return del(paths.dest);
+});
+
+gulp.task('styles', () => {
+  return gulp.src(paths.styles)
+      .pipe(sourcemaps.init())
+      .pipe(stylus({
+        compress: true,
+      }))
+      .pipe(rename('jquery.astrabox.min.css'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('scripts', () => {
+  return gulp.src(paths.scripts)
+    .pipe(concat('jquery.astrabox.min.js'))
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('default', gulp.series('clean', gulp.parallel('styles', 'scripts')));
