a2d6d7001405407b4f062a89f262b02e7955cfc1
fargelus <ddraudred@gmail.com>
2019-02-02 11:56:50 +0300

    Build static

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,23 @@ const rename = require('gulp-rename');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
 
+const browserSync = require('browser-sync').create();
+const flatten = require('gulp-flatten');
+
 const paths = {
   styles: './src/styl/*.styl',
   scripts: './src/js/*.js',
+  static: './static/**/*.{html,jpeg}',
   dest: './dist',
 };
 
 gulp.task('clean', () => del(paths.dest));
+
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: paths.dest,
+  });
+});
 
 gulp.task('styles', () => gulp.src(paths.styles)
   .pipe(sourcemaps.init())
@@ -33,4 +43,10 @@ gulp.task('scripts', () => gulp.src(paths.scripts)
   .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest(paths.dest)));
 
-gulp.task('default', gulp.series('clean', gulp.parallel('styles', 'scripts')));
+gulp.task('static', () => gulp.src(paths.static)
+  .pipe(flatten())
+  .pipe(gulp.dest(paths.dest)));
+
+// CLI
+gulp.task('build', gulp.parallel('static', 'styles', 'scripts'));
+gulp.task('prod', gulp.series('clean', 'build'));
